253f307513b071e167e7a749254f195117be90c5
Johnny Gérard <john@jgerard.dev>
2025-02-01 17:30:58 +0100

    refactor: remove redundant div

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,11 +1,7 @@
 import { memo } from "react";
 
 const NotFoundPage = () => {
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-    </div>
-  );
+  return <h1>404 Not Found</h1>;
 };
 
 export default memo(NotFoundPage);
