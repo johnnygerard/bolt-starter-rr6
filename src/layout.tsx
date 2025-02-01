1473b22d3a1d7c5f692371abdaf92bb468a597e1
Johnny Gérard <john@jgerard.dev>
2025-02-01 17:40:32 +0100

    feat: replace "Home" text with House icon

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,3 +1,4 @@
+import { House } from "@phosphor-icons/react";
 import { memo } from "react";
 import { Link, Outlet } from "react-router-dom";
 
@@ -7,7 +8,9 @@ const Layout = () => {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">
+              <House size="2rem" />
+            </Link>
           </li>
         </ul>
       </nav>
