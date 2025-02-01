f9e6beb39cf6189c44037c53c604b052ced335f1
Johnny Gérard <john@jgerard.dev>
2025-02-01 17:34:23 +0100

    feat: remove about page

--- src/app-router.tsx
+++ src/app-router.tsx
@@ -1,7 +1,6 @@
 import { memo } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout.tsx";
-import About from "./pages/about.tsx";
 import Home from "./pages/home.tsx";
 import NotFound from "./pages/not-found.tsx";
 
@@ -11,7 +10,6 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- src/layout.tsx
+++ src/layout.tsx
@@ -9,15 +9,8 @@ const Layout = () => {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>{" "}
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
         </ul>
       </nav>
-      <hr />
       <Outlet />
     </div>
   );
