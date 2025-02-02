5a57ec1177ace77b34b6ce6f387195112ecb33c0
Johnny Gérard <john@jgerard.dev>
2025-02-02 18:55:07 +0100

    feat: remove redundant button

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@heroui/button";
 import { memo } from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <Button>Click Me</Button>
     </div>
   );
 };
