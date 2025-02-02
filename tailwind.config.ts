35ef5548b947277e5971b067461990919ce8d76f
Johnny Gérard <john@jgerard.dev>
2025-02-02 02:35:44 +0100

    refactor: remove redundant comments

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{ts,tsx,mdx}",
-
-    // single component styles
     "./node_modules/@heroui/theme/dist/components/button.js",
-
-    // or you can use a glob pattern (multiple component styles)
-    // "./node_modules/@heroui/theme/dist/components/(button|snippet|code|input).js",
   ],
   theme: {
     extend: {},
