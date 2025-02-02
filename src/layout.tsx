7abddd16ba07f8e8a1d2ba0f6f926f56a4b37438
Johnny Gérard <john@jgerard.dev>
2025-02-02 22:18:22 +0100

    feat: update navigation bar

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,19 +1,20 @@
+import { Link } from "@heroui/link";
+import { Navbar, NavbarBrand } from "@heroui/navbar";
 import { House } from "@phosphor-icons/react";
 import { memo } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">
-              <House />
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar>
+        <NavbarBrand>
+          <Link href="/">
+            <House />
+            <span>AppName</span>
+          </Link>
+        </NavbarBrand>
+      </Navbar>
       <div className="grid flex-1 place-items-center">
         <Outlet />
       </div>
