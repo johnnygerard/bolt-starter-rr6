5181b5d7208e201a5e16e1886e8363d3d9e59e6b
Johnny Gérard <john@jgerard.dev>
2025-02-02 22:17:51 +0100

    build: install @heroui/link and @heroui/navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{ts,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
+    "./node_modules/@heroui/theme/dist/components/(button|link|navbar|ripple|spinner).js",
   ],
   theme: {
     extend: {},
